28bc2f601628c646c26a71c476b06828c2a4dae9
Bontempi Luca <luca@Lucas-Mac.local>
2020-11-30 08:25:52 +0100

    -

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ canvas.height = 0;
 let videoDevices = [];
 let defaultCamera = true;
 
-const queryString = window.location.search.slice(1).split("&&").split("%20").join(" ");
+const queryString = window.location.search.slice(1).split("&&").join(" ");
 param.innerHTML = queryString;
 
 takePicture.onclick = function () {
